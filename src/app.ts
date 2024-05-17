9b466a8e4bcdc8fbbc27847a0c6a853bde937af0
Kevin Mondragon <021000615@upsrj.edu.mx>
2024-05-16 23:18:35 -0600

    Dockerizado

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 import { createBot, MemoryDB } from '@builderbot/bot'
-import AIClass from './services/ai';
 import flow from './flows';
 // import { providerTelegram as provider } from './provider/telegram';
 import { provider } from './provider';
@@ -8,7 +7,6 @@ import { adapterDB } from './mongo-database';
 
 const PORT = 3301;
 //const PORT = process.env.PORT ?? 3301
-const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-3.5-turbo')
 
 const main = async () => {
 
@@ -16,7 +14,7 @@ const main = async () => {
         database: adapterDB,
         provider,
         flow,
-    }, { extensions: { ai } })
+    })
 
     httpServer(+PORT)
     console.log(`Ready for ${PORT}`)
